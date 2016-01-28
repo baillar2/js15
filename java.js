8bfddcf2045ba877db977f79f87c54f40b56cb0e
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-01-28 12:38:01 -0700

    fix the scroll

--- java.js
+++ java.js
@@ -1,19 +1,53 @@
+
+
 angular.module('moduleOne', [])
 
 angular.module('moduleOne')
 	.controller('controllerOne', ['$scope', function($scope){
-
 		$scope.day = []
+		$scope.week = [[], [],[]]
+		$scope.app = {}
+		$scope.week.push($scope.day)
+		document.addEventListener("scroll", function(event){
+				$scope.$apply(function(){
+				$scope.week.push([])
+				console.log(event)
+			})	
+			console.log('scroll worked')
+		})
+		//$scope.scroll = function () {
+		//	$scope.week.push($scope.day)	
+		//	console.log('scroll worked')
+//
 		$scope.showInput = function() {
 			$scope.dayInput = true
 		}
-		$scope.showList = function () {
+		$scope.hide = function(){
+			$scope.dayInput = false
+			console.log('blur works')
+		}
+		$scope.submit = function ($event) {
+			$scope.day.push($scope.app)
+			$scope.app = {}
 			$scope.list = true
+			$scope.dayInput = false
+			
 		}
 
+		$scope.edit = function (app) {
+			app.showEdit = true
+		}
+		$scope.editInfo = function (app) {
+			app.showEdit = false
+		}
 
+	
 	}])
 
-	// finish submit functin to push appointment into day[]
-	// add if(keypress.event.key#13 == enter key){then submit} 
-	// the data and trigger the ng-show on the list
+
+
+
+
+
+
+
